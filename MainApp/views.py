1329b507b2d0801ee3c7bc0d246a2c4f9a76e7fa
Ivan Sivolobov <bumstik1986@yandex.ru>
2025-11-08 14:09:56 +0300

    first task

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home_page(request):
+    author_name = 'Сиволобов И. В.'
+    html_content = f'''
+    <h1>Изучаем django</h1>
+    <strong>Автор</strong>: <i>{author_name}</i>
+    '''
+    return HttpResponse(html_content)
